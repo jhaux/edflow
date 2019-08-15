1fd4f3537430d7890234217c435f07b55fe4a683
Johannes Haux <jo.mobile.2011@gmail.com>
2019-08-15 17:05:18 +0200

    :memo: Explains how to release by hand and turns of jhaux specific code

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 
+
+# allows to get version via python setup.py --version
+__version__ = "0.2.1"
+
+
 setup(
     name="edflow",
-    version="0.2",
+    version=__version__,
     description="Logistics for Deep Learning",
     url="https://github.com/pesser/edflow",
     author="Mimo Tilbich et al.",
@@ -12,7 +17,7 @@ setup(
     package_data={"": ["*.yaml"]},
     install_requires=[
         "pyyaml",
-        "opencv-python",
+        "opencv",
         "tqdm",
         "Pillow",
         "chainer",
